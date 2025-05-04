ce35ae1f862ecd725ead750ce443bfd97d0721f6
aimed <aimededdine.touati@tech-instinct.com>
2025-05-04 23:03:46 +0100

    fix(docker-compose)

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,9 +1,9 @@
 -- Create databases
 CREATE DATABASE devdb;
-CREATE DATABASE proddb;
+--CREATE DATABASE proddb;
 
 -- Create development user
 CREATE USER devuser WITH PASSWORD 'devpassword';
 ALTER USER devuser WITH SUPERUSER;
 GRANT ALL PRIVILEGES ON DATABASE devdb TO devuser;
-GRANT ALL PRIVILEGES ON DATABASE proddb TO devuser;
+--GRANT ALL PRIVILEGES ON DATABASE proddb TO devuser;
